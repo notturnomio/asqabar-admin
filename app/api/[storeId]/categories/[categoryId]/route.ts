d86c3b4a7653b7178b72bbae7839be93122caa30
Rustem Mukazhanov <r.mukazhanov@gmail.com>
2023-08-04 23:24:57 +0200

    updated category route, added include banner

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -14,6 +14,9 @@ export async function GET(
       where: {
         id: params.categoryId,
       },
+      include: {
+        banner: true,
+      },
     });
 
     return NextResponse.json(category);
